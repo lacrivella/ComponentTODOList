bb68d9cd04b02c8223e108c23e3c1dac097626d2
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-05-23 14:55:50 -0700

    input eventlistener is working in console

--- src/component/Filter.js
+++ src/component/Filter.js
@@ -1,16 +1,39 @@
 import Component from './Component.js';
 
 class Filter extends Component {
+    render() {
+        const dom = this.renderDOM();
+        // const text = dom.querySelector('#text');
+        // const all = dom.querySelector('#all');
+        // const completed = dom.querySelector('#completed');
+        // const none = dom.querySelector('#none');
+        const inputs = dom.querySelectorAll('input');
+
+        inputs.forEach(input => {
+            input.addEventListener('input', () => {
+                //there is a good chance this is all wrong. lines 6-9 and 15-20
+                // this.props.onFilter({
+                    // text: text.value,
+                    // all: all.value,
+                    // completed: completed.value,
+                    // none: none.value
+                // });
+                console.log(input.value);
+            });
+        });
+        return dom;
+    }
+
     renderTemplate() {
         return /*html*/ `
-            <section class="filter-section">
+            <div class="filter-section">
+                <input id="text" name="filter">
                 <label class="radio-button">
-                    <input class="filter" name="filter">
-                    <input type="radio" name="radio"> All
-                    <input type="radio" name="radio"> Completed
-                    <input type="radio" name="radio"> Not Done
+                    <input type="radio" name="radio" id="all" value="all"> All
+                    <input type="radio" name="radio" id="completed" value="true"> Completed
+                    <input type="radio" name="radio" id="none" value="false"> Not Done
                 </label>
-            </section>
+            </div>
         `;
     }
 }
